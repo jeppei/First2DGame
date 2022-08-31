b89f68938d4ec8ca14ee82ba6ba04a991bc940fb
Jesper Lundin <jesper.l.lundin@gmail.com>
2022-08-31 23:09:31 +0200

    Add player animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float jumpVelocity;
-    public float runVelocity;
+    [SerializeField] private float jumpVelocity;
+    [SerializeField] private float runVelocity;
     [SerializeField] private LayerMask jumpableGround;
 
     private Rigidbody2D rigidBody;
     private BoxCollider2D boxCollider;
+    private Animator animator;
+    private SpriteRenderer sprite;
+
+    private enum AnimationState { idle, running, jumping, falling };
 
     // Start is called before the first frame update (when the game starts)
     private void Start()
     {
-        Debug.Log("Game started");
         rigidBody = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -30,6 +35,8 @@ public class PlayerMovement : MonoBehaviour
         {
             rigidBody.velocity = new Vector2(0, jumpVelocity);
         }
+
+        UpdateAnimation(dirX);
     }
 
     private bool IsGrounded()
@@ -42,5 +49,19 @@ public class PlayerMovement : MonoBehaviour
             .1f, 
             jumpableGround
         );
+    }
+
+    private void UpdateAnimation(float dirX)
+    {
+        if (dirX != 0) sprite.flipX = dirX < 0f;
+
+        AnimationState animationState =
+            (.1f < rigidBody.velocity.y)  ? AnimationState.jumping :
+            (rigidBody.velocity.y < -0.1) ? AnimationState.falling :
+            (dirX != 0f)                  ? AnimationState.running :
+            (dirX == 0f)                  ? AnimationState.idle :
+                                            AnimationState.idle;
+
+        animator.SetInteger("animationState", (int)animationState);
     }
 }
