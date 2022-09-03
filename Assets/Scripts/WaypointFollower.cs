174c8a552318cfee05d77252c27d2030774a9c6e
Jesper Lundin <jesper.l.lundin@gmail.com>
2022-09-03 14:45:15 +0200

    Add saw trap part 4 (final)

--- Assets/Scripts/WaypointFollower.cs
+++ Assets/Scripts/WaypointFollower.cs
@@ -9,18 +9,15 @@ public class WaypointFollower : MonoBehaviour
 
     [SerializeField] private float speed = 2f;
 
-    // Update is called once per frame
     void Update()
     {
+        if (waypoints.Length == 0) return;
+
         Vector2 nextWaypointPosition = waypoints[currentWaypointIndex].transform.position;
         if (Vector2.Distance(nextWaypointPosition, transform.position) < .1f) 
         {
             currentWaypointIndex++;
             currentWaypointIndex %= waypoints.Length;
-            //if (currentWaypointIndex >= waypoints.Length)
-            //{
-            //    currentWaypointIndex = 0;
-            //}
         }
         transform.position = Vector2.MoveTowards(transform.position, nextWaypointPosition, Time.deltaTime * speed);
     }
